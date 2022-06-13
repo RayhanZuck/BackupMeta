b4d51c1f1935eacd3f8862108078710109726b71
Rayhan Zuckerberg <76443273+RayhanZuck@users.noreply.github.com>
2022-06-13 17:46:46 +0700

    Add files via upload

--- lari.py
+++ lari.py
@@ -1,4 +1,5 @@
 #Tools Backup By Rayhan
+#Sc Buat Jalanin Sc Aja Di Recode LoL
 
 import os
 
@@ -10,19 +11,29 @@ B = '\x1b[1;96m' # BIRU
 
 os.system('git pull')
 os.system('clear')
-print(f"""
-{M}                                                  
-,---.          |              ,---,          |    
-|---',---.,   .|---.,---.,---. .-' .   .,---.|__/ 
-|  \ ,---||   ||   |,---||   ||    |   ||    |  \ 
-{P}`   ``---^`---|`   '`---^`   '`---'`---'`---'`   `
-          `---'                                   
----------------------------------------------------\n{K}Tools Created By Rayhan!\n\n{H}Github{P}  : github.com/RayhanZuck\n{H}Facebook{P}: fb.com/Rayhan.27.Xyz\n{H}Facebook{P}: fb.com/RayhanBusiness\n\n---------------------------------------------------\n{B}Ketika Crack Diharuskan Memasukkan Username\nDan Password, Masukkan Seperti Dibawah!\n\n{P}[{K}?{P}] Enter SC Username ({H}required{P}) : {H}meta\n{P}[{K}?{P}] Enter SC Password ({H}required{P}) : {H}metaroyid\n\n{M}Sc {P}Meta {M}Bukan Buatan Saya Melainkan ({P}Roy ID{M})!!\n""")
-
-print(f'{P}Ketik {H}Run {P}Untuk Masuk Ke Dalam Script\nKetik {M}Out {P}Untuk Keluar Dari Script')
-pilih = input(f'{K}\nKetik{P}: ')
-if pilih == 'run' or pilih == 'Run':
-      __import__("meta").cek_pw()
+print(f'-----------------------------------------------')
+print(f'| {K}Tools Ini Dibuat Oleh Rayhan, Good Job ! {P}   |')
+print(f'| Silahkan Baca Dan Pahami Ketentuan Dibawah  |')
+print(f'-----------------------------------------------')
+print(f'| Ketika Crack Diharuskan Memasukkan Username |')
+print(f'| Dan Password, Masukkan Seperti Dibawah!     |')
+print(f'-----------------------------------------------\n')
+print(f'{P}[{K}?{P}] Enter SC Username ({H}required{P}) : {H}meta')
+print(f'{P}[{K}?{P}] Enter SC Password ({H}required{P}) : {H}metaroyid\n')
+print(f'{P}--------------------------------------------------')
+print(f'{P}| Sc {K}Meta {P}Bukan Buatan Saya Melainkan ({K}Roy ID{P}) ! |')
+print(f'| Dan Sc {K}Meta{P} Telah Dihapus Ini Adalah Backupnya |')
+print(f'{P}--------------------------------------------------')
+print(f'| Sc Mungkin {M}Error {P}Jika Dibuka Dari Direktori |\n| Penyimpanan Atau Tidak Melalui Penginstalan |')
+print(f'{P}-----------------------------------------------------')
+print(f'| {K}Ingat Dan Jangan Lupakan {H}Username {K}Dan {H}Passwordnya{P} |')
+print(f'{P}-----------------------------------------------------')
+print(f'{P}| Jika Paham Ketik {H}Lanjut {P}Untuk Melanjutkan |')
+print(f'| Ketik {M}Out {P}Untuk Keluar Dari Script        |')
+print(f'{P}---------------------------------------------')
+pilih = input(f'{K}\nKetik{P}: {H}')
+if pilih == 'lanjut' or pilih == 'Lanjut':
+      os.system('python .1oJpCk54gQPxq02S.py')
 if pilih == 'out' or pilih == 'Out':
       print(f'{B}\nTerimakasih Telah Menggunakan Tools Ini Enjoy>_<\n')
-      
+      exit()
